b13cc5b2ba54df1cbe97afba28df08e0bfba567a
Steven-Abanto <steven.abanto.xd@gmail.com>
2025-03-13 20:56:48 -0500

    tercer commit

--- src/main/java/pe/edu/idat/demo_web_formularios/controller/ImcController.java
+++ src/main/java/pe/edu/idat/demo_web_formularios/controller/ImcController.java
@@ -12,10 +12,35 @@ public class ImcController {
     @GetMapping("/imc")
     public String formularioImc(Model model){
         model.addAttribute("imcmodel", new ImcModel());
+        model.addAttribute("visualizaralerta", false);
         return "imc";
     }
     @PostMapping("/calcularimc")
     public String calcuarImc(@ModelAttribute("imcmodel") ImcModel imc, Model model){
+        Double tallam = imc.getTalla()/100;
+        Double valorImc = imc.getPeso()/(tallam * tallam);
+        String diagnostico = "", estiloDiagnostico= "alert-danger";
+
+        if (valorImc <= 18.5){
+            diagnostico = "Por debajo del peso";
+            estiloDiagnostico = "alert-dark";
+        } else if (valorImc <= 25) {
+            diagnostico = "Con peso normal";
+            estiloDiagnostico = "alert-primary";
+        } else if (valorImc <= 30) {
+            diagnostico = "Con sobrepeso";
+            estiloDiagnostico = "alert-warning";
+        } else if (valorImc <= 36) {
+            diagnostico = "Obesidad leve";
+        } else if (valorImc <= 39) {
+            diagnostico = "Obesidad media";
+        }else{
+            diagnostico = "Obesidad mórbida";
+        }
+        model.addAttribute("Resultado","Su valor IMC = " + String.format("%.2f",valorImc) +
+                ", usted se encuentra: "+ diagnostico);
+        model.addAttribute("visualizaralerta", true);
+        model.addAttribute("estilodiagnostico", estiloDiagnostico);
         return "imc";
     }
 }
