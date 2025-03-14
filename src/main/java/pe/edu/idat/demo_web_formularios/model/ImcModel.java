38fb414e33cbfb0854b1da21e68558f3aafe7961
Steven-Abanto <steven.abanto.xd@gmail.com>
2025-03-13 19:49:46 -0500

    primer commit

---
+++ src/main/java/pe/edu/idat/demo_web_formularios/model/ImcModel.java
@@ -0,0 +1,22 @@
+package pe.edu.idat.demo_web_formularios.model;
+
+public class ImcModel {
+    public Double Talla;
+    public Double Peso;
+
+    public Double getTalla() {
+        return Talla;
+    }
+
+    public void setTalla(Double talla) {
+        Talla = talla;
+    }
+
+    public Double getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(Double peso) {
+        Peso = peso;
+    }
+}
